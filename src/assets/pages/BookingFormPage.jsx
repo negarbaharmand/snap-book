70033d2b2d026ebcbdc8100848376c2a463135c1
Negar Baharmand <negarbaharmand@outlook.com>
2023-12-12 15:03:52 +0100

    Create a new page for booking form

---
+++ src/assets/pages/BookingFormPage.jsx
@@ -0,0 +1,12 @@
+import BookingForm from "../components/BookingForm";
+
+const BookingFormPage = () => {
+  return (
+    <div>
+      <h2>Booking Form</h2>
+      <BookingForm />
+    </div>
+  );
+};
+
+export default BookingFormPage;
