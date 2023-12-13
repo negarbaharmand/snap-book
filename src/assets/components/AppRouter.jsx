c6db496e23980a52e56c5d5b5e17490f41fb8552
Negar Baharmand <negarbaharmand@outlook.com>
2023-12-13 09:09:48 +0100

    Update router

--- src/assets/components/AppRouter.jsx
+++ src/assets/components/AppRouter.jsx
@@ -1,19 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BookingFormPage from "../pages/BookingFormPage";
-import { BookingList } from "./BookingList";
-// import CancelBookingPage from "../pages/CancelBookingPage";
+import HomePage from "../pages/HomePage";
+import CancelBookingPage from "../pages/CancelBookingPage";
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<BookingList />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/booking-form" element={<></>} />
         <Route
           path="/booking-form/:id/:date/:time"
           element={<BookingFormPage />}
         />
-        {/* <Route path="/cancel-booking/:bookingId" element={<CancelBookingPage />} /> */}
+        <Route path="/cancel" element={<CancelBookingPage />} />
+        <Route path="/booking" element={<BookingFormPage />} />
       </Routes>
     </Router>
   );
