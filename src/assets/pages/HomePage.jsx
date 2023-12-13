3bb5be1b664207213a7b200521fe811e0871bb6b
Negar Baharmand <negarbaharmand@outlook.com>
2023-12-13 09:08:25 +0100

    Create HomePage

---
+++ src/assets/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import { BookingList } from "../components/BookingList";
+import Header from "../components/Header";
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <BookingList />
+    </>
+  );
+};
+
+export default HomePage;
