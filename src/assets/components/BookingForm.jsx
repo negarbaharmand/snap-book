c505ba55bce7a2a2bd9f448f08de318a761a1f86
Negar Baharmand <negarbaharmand@outlook.com>
2023-12-14 18:04:44 +0100

    Add Toaster to BookingForm and implement it on success

--- src/assets/components/BookingForm.jsx
+++ src/assets/components/BookingForm.jsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { Toaster, toast } from "sonner";
 
 const BookingForm = ({ id, date, time }) => {
   const baseURL = "http://localhost:8080";
   const [email, setEmail] = useState("");
+  const [bookingSuccess, setBookingSuccess] = useState(false);
   const navigate = useNavigate();
 
   const onBookingSuccess = () => {
     console.log("Booking successful!");
-    alert("Booking successful! Thank you for booking.");
-    navigate("/");
+    setBookingSuccess(true);
+    toast.success("Your ID has been saved in your clipboard!", {
+      position: "top-center",
+      onAutoClose: () => navigate("/"),
+    });
+    // alert("Success! Your ID has been saved in your clipboard!");
+    // navigate("/");
   };
 
   const bookAppointment = async () => {
@@ -20,6 +27,7 @@ const BookingForm = ({ id, date, time }) => {
         email,
       });
       if (response.status === 201) {
+        navigator.clipboard.writeText(id);
         onBookingSuccess();
       }
     } catch (error) {
@@ -37,50 +45,60 @@ const BookingForm = ({ id, date, time }) => {
   };
 
   return (
-    <div className="container-sm mt-5">
-      <div className="card">
-        <div className="card-header bg-primary text-white ">
-          <h3 className="card-title">Booking</h3>
-        </div>
-        <div className="card-body">
-          <div className="form-group mb-3">
-            <label htmlFor="email" className="form-label">
-              Email Address:
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              placeholder="Enter Your Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <small id="emailHelp" className="form-text text-muted">
-              We will never share your email with anyone else.
-            </small>
+    <>
+      <Toaster
+        toastOptions={{
+          style: {
+            background: "#d4edda",
+          },
+        }}
+      />
+      <div className="container-sm mt-5">
+        <div className="card">
+          <div className="card-header bg-dark text-white">
+            <h3 className="card-title">Booking</h3>
           </div>
-          <button
-            type="button"
-            className="btn btn-primary"
-            onClick={bookAppointment}
-          >
-            Book
-          </button>
-          <br />
-          <br />
-          <div className="border border-primary border-opacity-50 border-4 max-w-xs p-3 p-3">
-            <h3 className="card-title">Booking Details</h3>
-            <div className="card-body">
-              <div className="mb-3">
-                <p>ID: {id}</p>
-                <p>Date: {date}</p>
-                <p>Time: {time}</p>
+          <div className="card-body">
+            <div className="form-group mb-3">
+              <label htmlFor="email" className="form-label">
+                Email Address:
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Enter Your Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <small id="emailHelp" className="form-text text-muted">
+                We will never share your email with anyone else.
+              </small>
+            </div>
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={bookAppointment}
+              disabled={bookingSuccess}
+            >
+              Book
+            </button>
+            <br />
+            <br />
+            <div className="border max-w-xs p-3 p-3 alert alert-light">
+              <h3 className="card-title">Booking Details</h3>
+              <div className="card-body">
+                <div className="mb-3">
+                  <p>ID: {id}</p>
+                  <p>Date: {date}</p>
+                  <p>Time: {time}</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
